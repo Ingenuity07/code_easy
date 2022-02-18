e687f1c54956d69fc7d2cd210907783d85fc8943
Ingenuity07 <shivamsi103@gmail.com>
2022-02-18 13:04:35 +0530

    fixed route in resource component

--- client/src/Components/Resources.js
+++ client/src/Components/Resources.js
@@ -60,7 +60,7 @@ const Resources = ({ parent, setEdit, edit, admin }) => {
     return (
 
         <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-
+            <h2 style={{ color: "white" }}>{parent}</h2>
             {admin && <Link to={'/addResource/' + id}>
                 <div className="card card-resource resource" style={{ height: "4rem", borderBlockColor: "#674cff" }} >
                     <h4>Add More</h4>
@@ -68,9 +68,9 @@ const Resources = ({ parent, setEdit, edit, admin }) => {
             </Link>}
 
             {error && <div>{error}</div>}
-            {data&&data.length>0 && (
+            {data && data.length > 0 && (
                 <article >
-                    <h2 style={{ color: "white" }}>{parent}</h2>
+
                     <div className="cards cards-resource">
                         <div className="search">
                             <i className="fas fa-search" style={{ color: "white", marginRight: "5px", fontSize: "1.5rem" }}></i>
