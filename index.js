59654d4dcbc93ec458a3859442cf5d0242301e03
Ingenuity07 <shivamsi103@gmail.com>
2022-02-18 15:48:33 +0530

    server index js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const adminRoutes = require('./Routes/adminRoutes')
 app.use("/course",courseRoutes)
 app.use("/admin",adminRoutes)
 app.get("/*",(req,res)=>{
-    res.sendFile(path.join(client, './public/index.html'), function(err) {
+    res.sendFile(path.join("client/public", 'index.html'), function(err) {
         if (err) {
           res.status(500).send(err)
         }
