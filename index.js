a19a6ec2f12017ef7e4854d56eb68babcf48e777
Ingenuity07 <shivamsi103@gmail.com>
2022-05-12 15:11:52 +0530

    wildcard router added in server

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ const adminRoutes = require('./Routes/adminRoutes')
 app.use("/course",courseRoutes)
 app.use("/admin",adminRoutes)
 
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, './client/public/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
+
 if(process.env.NODE_ENV == "production")
 {
     app.use(express.static("client/build"))
